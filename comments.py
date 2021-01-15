ab4f51f9649b78e008876b7adc8ee9b9b50a6d18
Edward Boland <mr.eboland@gmail.com>
2021-01-14 19:20:28 -0500

    brief comments lesson

---
+++ comments.py
@@ -0,0 +1,4 @@
+# Say hello to everyone.
+print("Hello Python people!")
+
+# We write comments in order to explain our code and how it works to others and ourselves (if we leave it alone for awhile).
